16e42dc9ef823a1f3da20060cf29c1f3f2dc54de
setterwall <RobinSetterwall@n155-p36.kthopen.kth.se>
2015-03-27 14:21:46 +0100

    More

--- app.js
+++ app.js
@@ -8,5 +8,26 @@ function initialize() {
   };
   map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
 
+google.maps.event.addDomListener(zoomIn, 'click', zoomControl("+"));
+google.maps.event.addDomListener(zoomOut, 'click', zoomControl("-"));
 }
 google.maps.event.addDomListener(window, 'load', initialize);
+
+function zoomControl(zoomType){
+var currentZoomLevel = map.getZoom();
+if (zoomType=='+'){
+	if(currentZoomLevel != 21){
+		map.setZoom(currentZoomLevel + 1);
+	}
+
+}
+if (zoomType=='-'){
+
+	if(currentZoomLevel != 0){
+		map.setZoom(currentZoomLevel - 1);}
+
+
+}
+
+}
+
