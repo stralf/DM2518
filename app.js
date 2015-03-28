d947c4527a17de4371e7feb8122aca32fba2325d
setterwall <RobinSetterwall@MacBook-Air.local>
2015-03-28 13:28:35 +0100

    Moar

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var map;
+
 function initialize() {
   var mapProp = {
     center:new google.maps.LatLng(59.347327,18.073537),
@@ -8,26 +9,49 @@ function initialize() {
   };
   map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
 
-google.maps.event.addDomListener(zoomIn, 'click', zoomControl("+"));
-google.maps.event.addDomListener(zoomOut, 'click', zoomControl("-"));
+		google.maps.event.addDomListener(zoomIn, 'click', zoomingIn);
+		google.maps.event.addDomListener(zoomOut, 'click', zoomingOut);
+		google.maps.event.addDomListener(left, 'click', panLeft);
+		google.maps.event.addDomListener(right, 'click', panRight);
+		google.maps.event.addDomListener(up, 'click', panDown);
+		google.maps.event.addDomListener(down, 'click', panUp);
 }
+
 google.maps.event.addDomListener(window, 'load', initialize);
 
-function zoomControl(zoomType){
-var currentZoomLevel = map.getZoom();
-if (zoomType=='+'){
-	if(currentZoomLevel != 21){
-		map.setZoom(currentZoomLevel + 1);
-	}
+function zoomingIn() {
+   		var currentZoomLevel = map.getZoom();
+  		 if(currentZoomLevel != 21){
+     			map.setZoom(currentZoomLevel + 1);}
+ };
 
+function zoomingOut() {
+   		var currentZoomLevel = map.getZoom();
+  		 if(currentZoomLevel != 0){
+     			map.setZoom(currentZoomLevel - 1);}
+ };
+
+
+function panLeft(){
+		var center = map.getCenter();
+		var new_center = new google.maps.LatLng(center.lat(), center.lng() - 0.01); 
+		map.setCenter(new_center);
 }
-if (zoomType=='-'){
 
-	if(currentZoomLevel != 0){
-		map.setZoom(currentZoomLevel - 1);}
-
-
+function panRight(){
+		var center = map.getCenter();
+		var new_center = new google.maps.LatLng(center.lat(), center.lng() + 0.01); 
+		map.setCenter(new_center);
 }
 
+function panUp(){
+		var center = map.getCenter();
+		var new_center = new google.maps.LatLng(center.lat() - 0.01, center.lng()); 
+		map.setCenter(new_center);
 }
 
+function panDown(){
+		var center = map.getCenter();
+		var new_center = new google.maps.LatLng(center.lat() + 0.01, center.lng()); 
+		map.setCenter(new_center);
+}
