8e522eccbb2dac724833b863401d5872dd877746
stralf <annika.stralfors@gmail.com>
2015-03-30 22:28:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 var map;
+//testing
 
 function initialize() {
   var mapProp = {
-    center:new google.maps.LatLng(59.347327,18.073537),
-    zoom:16,
-    mapTypeId:google.maps.MapTypeId.TERRAIN,
-    disableDefaultUI: true
-  };
-  map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
+	    center:new google.maps.LatLng(59.347327,18.073537),
+	    zoom:16,
+	    mapTypeId:google.maps.MapTypeId.TERRAIN,
+	    disableDefaultUI: true
+	  };
 
+	  map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+		marker_draggable = new google.maps.Marker({
+	    map:map,
+	    draggable:true,
+	    animation: google.maps.Animation.DROP,
+	    position: new google.maps.LatLng(59.347327,18.073537)
+	  });
+
+		marker_not_draggable = new google.maps.Marker({
+	    map:map,
+	    draggable:false,
+	    position: new google.maps.LatLng(59.347327,18.073537)
+	  });
+
+	  google.maps.event.addListener(marker_draggable, 'click', toggleBounce);
 		google.maps.event.addDomListener(zoomIn, 'click', zoomingIn);
 		google.maps.event.addDomListener(zoomOut, 'click', zoomingOut);
 		google.maps.event.addDomListener(left, 'click', panLeft);
@@ -24,6 +40,16 @@ function zoomingIn() {
   		 if(currentZoomLevel != 21){
      			map.setZoom(currentZoomLevel + 1);}
  };
+
+function toggleBounce() {
+
+  if (marker_draggable.getAnimation() != null) {
+    marker_draggable.setAnimation(null);
+  } else {
+    marker_draggable.setAnimation(google.maps.Animation.BOUNCE);
+  }
+}
+
 
 function zoomingOut() {
    		var currentZoomLevel = map.getZoom();
